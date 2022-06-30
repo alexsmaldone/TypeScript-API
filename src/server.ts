fe3f6bc4db415786f72ac8312f9df6956e4218e3
alexsmaldone <alexsmaldone@gmail.com>
2022-06-30 14:13:39 -0400

    setup basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("hello wold")
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request,res: Response) => {
+  return res.send("Hello World");
+})
+
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
