55f411b2b3efc77f674216ac97e53614a6f0a8a5
alexsmaldone <alexsmaldone@gmail.com>
2022-06-30 22:44:59 -0400

    sqlite downloaded, added routes for students

--- src/routes/api/students.ts
+++ src/routes/api/students.ts
@@ -18,6 +18,17 @@ router.get("/", async (req: Request,res: Response) => {
 }
 );
 
+// GET route that queries the database and returns a single student
+router.get("/:id", async (req: Request,res: Response) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+    return res.send(student)
+  } catch (e) {
+    return res.json({msg: "Error finding student", status: 500, route: '/api/students/:id'});
+  }
+}
+);
+
 // POST route that creates a new student in the database
 router.post("/", async (req: Request,res: Response) => {
   console.log(req.body);
